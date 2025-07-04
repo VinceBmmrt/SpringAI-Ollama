8afad150e511a29dc2809ede7cb3a847c773f80b
VinceBmmrt <vincebommert75@gmail.com>
2025-07-04 16:12:37 +0200

    Initial commit

---
+++ src/main/java/com/telusko/SpringAIOllama/OllamaController.java
@@ -0,0 +1,48 @@
+package com.telusko.SpringAIOllama;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class OllamaController {
+
+    private ChatClient chatClient;
+    private ChatMemory chatMemory;
+
+    public OllamaController(OllamaChatModel chatModel) {
+        this.chatClient = ChatClient.create(chatModel);
+    }
+
+//    public OllamaController(ChatClient.Builder builder) {
+//        this.chatMemory = MessageWindowChatMemory.builder().build();
+//        this.chatClient = builder
+//                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+//                .build();
+//    }
+
+    @GetMapping("/api/{message}")
+    public ResponseEntity<String> getAnswer(@PathVariable String message) {
+
+        ChatResponse chatResponse = chatClient
+                .prompt(message)
+                .call()
+                .chatResponse();
+
+        System.out.println(chatResponse.getMetadata().getModel());
+
+        String response = chatResponse
+                .getResult()
+                .getOutput()
+                .getText();
+
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ src/main/java/com/telusko/SpringAIOllama/OpenAIController.java
@@ -0,0 +1,47 @@
+package com.telusko.SpringAIOllama;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class OpenAIController {
+
+    private ChatClient chatClient;
+    private ChatMemory chatMemory;
+//    public OpenAIController(OpenAiChatModel chatModel) {
+//        this.chatClient = ChatClient.create(chatModel);
+//    }
+
+//    public OpenAIController(ChatClient.Builder builder) {
+//
+//        this.chatMemory = MessageWindowChatMemory.builder().build();
+//
+//        this.chatClient = builder
+//                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+//                .build();
+//    }
+
+    //    @GetMapping("/api/{message}")
+    public ResponseEntity<String> getAnswer(@PathVariable String message) {
+
+        ChatResponse chatResponse = chatClient
+                .prompt(message)
+                .call()
+                .chatResponse();
+
+        System.out.println(chatResponse.getMetadata().getModel());
+
+        String response = chatResponse
+                .getResult()
+                .getOutput()
+                .getText();
+
+        return ResponseEntity.ok(response);
+    }
+}
