88415ac38dcc34d05c4197ef5e46e528719881a7
VinceBmmrt <vincebommert75@gmail.com>
2025-07-08 17:00:42 +0200

    PG Vector Store Implementation: - Replaced embedding model with OpenAI's ada-002 - Updated PgVectorStore vector dimension accordingly

--- src/main/java/com/telusko/SpringAIOllama/config/AppConfig.java
+++ src/main/java/com/telusko/SpringAIOllama/config/AppConfig.java
@@ -1,17 +1,23 @@
 package com.telusko.SpringAIOllama.config;
 
 import org.springframework.ai.embedding.EmbeddingModel;
-import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public VectorStore vectorStore(@Qualifier("openAiEmbeddingModel") EmbeddingModel embeddingModel) {
-        return SimpleVectorStore.builder(embeddingModel).build();
+
+    public VectorStore vectorStore(JdbcTemplate jdbcTemplate, @Qualifier("openAiEmbeddingModel") EmbeddingModel embeddingModel) {
+        return PgVectorStore.builder(jdbcTemplate, embeddingModel)
+                .dimensions(3072)
+                .distanceType(PgVectorStore.PgDistanceType.COSINE_DISTANCE)
+                .indexType(PgVectorStore.PgIndexType.HNSW)
+                .build();
     }
 }
--- src/main/java/com/telusko/SpringAIOllama/controller/OllamaController.java
+++ src/main/java/com/telusko/SpringAIOllama/controller/OllamaController.java
@@ -1,24 +1,24 @@
-package com.telusko.SpringAIOllama.controller;
-
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.ollama.OllamaChatModel;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@CrossOrigin(origins = "*")
-public class OllamaController {
-
-    private ChatClient chatClient;
-    private ChatMemory chatMemory;
-
-    public OllamaController(OllamaChatModel chatModel) {
-        this.chatClient = ChatClient.create(chatModel);
-    }
+//package com.telusko.SpringAIOllama.controller;
+//
+//import org.springframework.ai.chat.client.ChatClient;
+//import org.springframework.ai.chat.memory.ChatMemory;
+//import org.springframework.ai.chat.model.ChatResponse;
+//import org.springframework.ai.ollama.OllamaChatModel;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@CrossOrigin(origins = "*")
+//public class OllamaController {
+//
+//    private ChatClient chatClient;
+//    private ChatMemory chatMemory;
+//
+//    public OllamaController(OllamaChatModel chatModel) {
+//        this.chatClient = ChatClient.create(chatModel);
+//    }
 
 //    public OllamaController(ChatClient.Builder builder) {
 //        this.chatMemory = MessageWindowChatMemory.builder().build();
@@ -27,21 +27,21 @@ public class OllamaController {
 //                .build();
 //    }
 
-    //    @GetMapping("/api/{message}")
-    public ResponseEntity<String> getAnswer(@PathVariable String message) {
-
-        ChatResponse chatResponse = chatClient
-                .prompt(message)
-                .call()
-                .chatResponse();
-
-        System.out.println(chatResponse.getMetadata().getModel());
-
-        String response = chatResponse
-                .getResult()
-                .getOutput()
-                .getText();
-
-        return ResponseEntity.ok(response);
-    }
-}
+//    @GetMapping("/api/{message}")
+//    public ResponseEntity<String> getAnswer(@PathVariable String message) {
+//
+//        ChatResponse chatResponse = chatClient
+//                .prompt(message)
+//                .call()
+//                .chatResponse();
+//
+//        System.out.println(chatResponse.getMetadata().getModel());
+//
+//        String response = chatResponse
+//                .getResult()
+//                .getOutput()
+//                .getText();
+//
+//        return ResponseEntity.ok(response);
+//    }
+//}
